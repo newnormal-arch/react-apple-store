75525a411d7c884986529f75a9557b0b8a50705f
Bongz Podolski <ntshingilab@outlook.com>
2023-11-18 19:27:42 +0200

    search funtionality done

--- src/data/products.js
+++ src/data/products.js
@@ -4,9 +4,7 @@ import BlackiPhone11 from '../assets/images/iphone11-black.png';
 import RediPhone11 from '../assets/images/iphone11-red.png';
 import RediPhone13 from '../assets/images/iphone13-red.png';
 import RediPhone14 from '../assets/images/iphone14-red.png';
-import Macbook2 from '../assets/images/mac2.png';
-import MacStudio from '../assets/images/studio.png'
-import SubwayiPhone11 from '../assets/images/iphone11-subway.png'
+import SubwayiPhone11 from '../assets/images/iphone11-subway.png';
 import WhiteiPhone11 from '../assets/images/iphone11-white.png';
 
 export const productList = [
@@ -81,32 +79,5 @@ export const productList = [
         "long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim odio faucibus nec malesuada purus volutpat vel sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, sit nunc, nibh purus pellentesque sagittis. Felis rhoncus facilisis massa eget purus in purus. Etiam at cras nulla nunc. Malesuada in pretium diam scelerisque sit mattis in egestas neque. Eu porta tempor sodales nisl integer turpis porttitor sed sed. Ut senectus odio dictum enim velit tempor diam quisque suspendisse.Orci vel ridiculus diam viverra. Libero malesuada orci, quis placerat suscipit augue imperdiet. Et praesent augue dictum mauris eget lacus malesuada. Aenean nisi, sodales natoque massa magna dignissim mi. Mattis tellus, justo, lorem sed tempor diam sit viverra enim. Id id placerat eu etiam nulla laoreet. Dignissim leo fames turpis quis suspendisse vulputate laoreet vulputate ac. Aliquam justo lectus eu dui porttitor. Cras a aliquam phasellus sollicitudin ornare. Tristique volutpat facilisis in ut proin. Est vitae facilisi sollicitudin id lorem mattis nibh ipsum, nec. Consectetur consectetur morbi morbi aliquet mi risus, velit, sit at. Integer morbi viverra hendrerit risus. Odio phasellus nibh senectus nec id enim quam sed. At potenti sollicitudin sollicitudin lobortis morbi. Nunc molestie et adipiscing aliquam. Sit vel mi dolor suscipit. In eget ut ac at facilisi leo viverra. Arcu ac ut fermentum, viverra et, vitae etiam cras. Eu purus non ut turpis fusce. Mi vitae nibh mi ut feugiat varius risus eros.",
         "imageUrl": RediPhone14,
         "price": "1099.99"
-    },
-    {
-        "id": 9,
-        "name": "Macbook",
-        "rating": "4.7",
-        "short_description": "Air 16GB",
-        "long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim odio faucibus nec malesuada purus volutpat vel sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, sit nunc, nibh purus pellentesque sagittis. Felis rhoncus facilisis massa eget purus in purus. Etiam at cras nulla nunc. Malesuada in pretium diam scelerisque sit mattis in egestas neque. Eu porta tempor sodales nisl integer turpis porttitor sed sed. Ut senectus odio dictum enim velit tempor diam quisque suspendisse.Orci vel ridiculus diam viverra. Libero malesuada orci, quis placerat suscipit augue imperdiet. Et praesent augue dictum mauris eget lacus malesuada. Aenean nisi, sodales natoque massa magna dignissim mi. Mattis tellus, justo, lorem sed tempor diam sit viverra enim. Id id placerat eu etiam nulla laoreet. Dignissim leo fames turpis quis suspendisse vulputate laoreet vulputate ac. Aliquam justo lectus eu dui porttitor. Cras a aliquam phasellus sollicitudin ornare. Tristique volutpat facilisis in ut proin. Est vitae facilisi sollicitudin id lorem mattis nibh ipsum, nec. Consectetur consectetur morbi morbi aliquet mi risus, velit, sit at. Integer morbi viverra hendrerit risus. Odio phasellus nibh senectus nec id enim quam sed. At potenti sollicitudin sollicitudin lobortis morbi. Nunc molestie et adipiscing aliquam. Sit vel mi dolor suscipit. In eget ut ac at facilisi leo viverra. Arcu ac ut fermentum, viverra et, vitae etiam cras. Eu purus non ut turpis fusce. Mi vitae nibh mi ut feugiat varius risus eros.",
-        "imageUrl": Macbook2,
-        "price": "1499.99"
-    },
-    {
-        "id": 10,
-        "name": "Macbook",
-        "rating": "4.8",
-        "short_description": "Air 8GB",
-        "long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim odio faucibus nec malesuada purus volutpat vel sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, sit nunc, nibh purus pellentesque sagittis. Felis rhoncus facilisis massa eget purus in purus. Etiam at cras nulla nunc. Malesuada in pretium diam scelerisque sit mattis in egestas neque. Eu porta tempor sodales nisl integer turpis porttitor sed sed. Ut senectus odio dictum enim velit tempor diam quisque suspendisse.Orci vel ridiculus diam viverra. Libero malesuada orci, quis placerat suscipit augue imperdiet. Et praesent augue dictum mauris eget lacus malesuada. Aenean nisi, sodales natoque massa magna dignissim mi. Mattis tellus, justo, lorem sed tempor diam sit viverra enim. Id id placerat eu etiam nulla laoreet. Dignissim leo fames turpis quis suspendisse vulputate laoreet vulputate ac. Aliquam justo lectus eu dui porttitor. Cras a aliquam phasellus sollicitudin ornare. Tristique volutpat facilisis in ut proin. Est vitae facilisi sollicitudin id lorem mattis nibh ipsum, nec. Consectetur consectetur morbi morbi aliquet mi risus, velit, sit at. Integer morbi viverra hendrerit risus. Odio phasellus nibh senectus nec id enim quam sed. At potenti sollicitudin sollicitudin lobortis morbi. Nunc molestie et adipiscing aliquam. Sit vel mi dolor suscipit. In eget ut ac at facilisi leo viverra. Arcu ac ut fermentum, viverra et, vitae etiam cras. Eu purus non ut turpis fusce. Mi vitae nibh mi ut feugiat varius risus eros.",
-        "imageUrl": Macbook2,
-        "price": "1199.99"
-    },
-    {
-        "id": 11,
-        "name": "Mac",
-        "rating": "4.0",
-        "short_description": "Studio 16GB",
-        "long_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim odio faucibus nec malesuada purus volutpat vel sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, sit nunc, nibh purus pellentesque sagittis. Felis rhoncus facilisis massa eget purus in purus. Etiam at cras nulla nunc. Malesuada in pretium diam scelerisque sit mattis in egestas neque. Eu porta tempor sodales nisl integer turpis porttitor sed sed. Ut senectus odio dictum enim velit tempor diam quisque suspendisse.Orci vel ridiculus diam viverra. Libero malesuada orci, quis placerat suscipit augue imperdiet. Et praesent augue dictum mauris eget lacus malesuada. Aenean nisi, sodales natoque massa magna dignissim mi. Mattis tellus, justo, lorem sed tempor diam sit viverra enim. Id id placerat eu etiam nulla laoreet. Dignissim leo fames turpis quis suspendisse vulputate laoreet vulputate ac. Aliquam justo lectus eu dui porttitor. Cras a aliquam phasellus sollicitudin ornare. Tristique volutpat facilisis in ut proin. Est vitae facilisi sollicitudin id lorem mattis nibh ipsum, nec. Consectetur consectetur morbi morbi aliquet mi risus, velit, sit at. Integer morbi viverra hendrerit risus. Odio phasellus nibh senectus nec id enim quam sed. At potenti sollicitudin sollicitudin lobortis morbi. Nunc molestie et adipiscing aliquam. Sit vel mi dolor suscipit. In eget ut ac at facilisi leo viverra. Arcu ac ut fermentum, viverra et, vitae etiam cras. Eu purus non ut turpis fusce. Mi vitae nibh mi ut feugiat varius risus eros.",
-        "imageUrl": MacStudio,
-        "price": "999.99"
     },
 ]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import {productList} from "../data/products";
 import SideNavBar from "../components/navbar/SideNavBar";
 import HomeProductCard from "../components/home/HomeProductCard";
 import CartSideBar from "../components/cart/CartSideBar";
 
-
 function Home() {
+    const [search, setSearch] = useState("");
+    console.log(search);
     return (        
         <div className="home-page">
             <SideNavBar />
@@ -13,10 +14,12 @@ function Home() {
             <div className="home-container">
                 <div className="search-container">
                     <h6 className="search-heading">Search Item</h6>
-                    <input className="search-input" placeholder="Apple Watch, Samsung S21, Macbook Pro, ..."></input>
+                    <input onChange={(e) => setSearch(e.target.value)} className="search-input" placeholder="Apple Watch, Samsung S21, Macbook Pro, ..."></input>
                 </div>
                 <div className="product-list">
-                {productList.map(item => {
+                {productList.filter((item) => {
+                    return search.toLowerCase() === '' ? item : item.name.toLowerCase().includes(search)
+                }).map(item => {
                     return (
                         <div key={item.id}>
                             <HomeProductCard data={item} />
