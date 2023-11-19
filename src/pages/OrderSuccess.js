25cef8801937732bfd24420f82167176ef171d19
Bongz Podolski <ntshingilab@outlook.com>
2023-11-19 12:18:41 +0200

    ux change

--- src/pages/OrderSuccess.js
+++ src/pages/OrderSuccess.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { TiTickOutline } from "react-icons/ti";
+import { useNavigate } from "react-router-dom";
 
 function SuccessfullOrder(){
+    const navigate = useNavigate();
+
+    const handleReload = (e) => {
+        e.preventDefault();
+        navigate('/');
+        window.location.reload();
+    }
     return (
         <div className="success-card">
             <i><TiTickOutline /></i>
             <h2>Thank You</h2>
             <p>Your order has been confirmed</p>
+            <button className="success-btn" onClick={handleReload}>Back To Homepage</button>
         </div>
     );
 }
